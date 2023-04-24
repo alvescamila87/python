59bf043c999bce7242a466cce2d294ac91aa9e91
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-04-24 20:29:01 -0300

    update python conditions if else

---
+++ CursoEmVideo/Aulas/aula10_Conditions.py
@@ -0,0 +1,20 @@
+// Conditions
+
+carro.siga()
+if carro.esquerda()
+    -
+    -
+    -
+else
+    -
+    -
+    -
+carro.pare()
+
+feito por ordenação identada
+    if:
+        bloco verde (verdadeiro)
+    else:
+        bloco vermelho (falso)
+nunca serão executados os dois blocos, somente um.
+
---
+++ CursoEmVideo/pythonTest/aula10.py
@@ -0,0 +1,5 @@
+tempo=int(input('quanto tempo tem seu carro?'))
+if tempo <=3:
+    print('Seu carro é novo')
+else:
+    print('Seu carro é velho')
