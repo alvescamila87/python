ba3a635d3157ba1d3004b7e7c2ca1b0b6f2c5fae
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-02 20:17:55 -0300

    update exercicio quadrado módulo 2 USP

---
+++ USP/exercicios/quadrado.py
@@ -0,0 +1,4 @@
+quadrado = float(input('Digite o valor correspondente ao lado de um quadrado: '))
+perimetro = quadrado * 4
+area = quadrado ** 2
+print('Perímetro: {:.2f} e Área {:.2f}'.format(perimetro, area))
---
+++ USP/modulo01_expressions.py
@@ -0,0 +1,17 @@
+// expressoes
+
+- expressões aritméticas:
+    operadores:  +-*/
+    operandos: números
+
+- expressões relacionais
+    ==
+    !==
+    ...
+    boolean (true or false)
+
+- expressões lógicas
+    valores lógicos sobre valores booleanos
+    not
+    and
+    or
