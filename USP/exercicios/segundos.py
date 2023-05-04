bd53e750352b2304741df88f7b1b7fa55b0315cd
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-04 20:31:04 -0300

    update exercicio de segundo USP

--- USP/exercicios/segundos.py
+++ USP/exercicios/segundos.py
@@ -1,6 +1,16 @@
 segundos=int(input('Por favor, entre com o número de segundos que deseja converter: '))
-a= segundos // 35760
-b= segundos % 3600
-c= segundos // 60
-d= segundos % 60
+a = segundos // 86400
+segundos_rest = segundos % 86400
+b = segundos_rest // 3600
+segundos_rest = segundos_rest % 3600
+c = segundos_rest // 60
+d = segundos_rest % 60
 print('{} dias, {} horas, {} minutos e {} segundos.'.format(a, b, c, d))
+
+#forma de cálculo
+#pega o total de segundos e divide por 86400 (a quantidade de segundos em um dia), o resultado (descontando as casas decimais) é o total de dias
+#pega o resto da divisão por 86400, assim tem-se a "sobra" (a quantidade de segundos que não é suficiente para completar um dia)
+#depois divide esse resultado por 3600 (a quantidade de segundos em uma hora), o resultado (descontando as casas decimais) é o total de horas
+#pega o resto da divisão por 3600, assim tem-se a "sobra" (a quantidade de segundos que não é suficiente para completar uma hora)
+#depois divide esse resultado por 60 (a quantidade de segundos em um minuto), o resultado (descontando as casas decimais) é o total de minutos
+#pega o resto da divisão por 60, assim tem-se a "sobra" (a quantidade de segundos que não é suficiente para completar um minuto)
