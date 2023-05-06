97a4c4184c72336fef50e636def3093d82125497
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-05 21:02:05 -0300

    update import math python

---
+++ USP/ex002.py
@@ -0,0 +1,26 @@
+import math
+
+#fazer programa que calcule as raízes de 2º
+#equação quadrática
+#calcular apenas raízes reais
+#se delta menor que zero, 'essa equação não possui raizes reais {}'
+#se delta for igual a zero, 'essa equação possui raizes reais {}'
+#se delta for maior a zero, 'as raízes dessa equação são: 1ª raiz: {} e 2ª raiz: {}'
+#ax2 + bx + c = 0
+
+math.sqrt(8)
+print()
+
+a = int(input('Informe o valor de a:'))
+b = int(input('Informe o valor de b: '))
+c = int(input('Informa o valor de c:' ))
+
+delta = (b ** 2) - 4 * a * c
+
+if delta < 0:
+    print('Essa equação não possui raizes reais {}'.format(delta))
+    if delta == 0:
+        print('Essa equação possui raizes reais {}'.format(delta))
+    if delta > 0:
+        print('As raízes dessa equação são: 1ª raiz: {} e 2ª raiz: {}'.format(delta, delta))
+
--- USP/programa.py
+++ USP/programa.py
@@ -1,2 +1,14 @@
 print("mensagem")
 
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+soma = a + b
+print("A soma de a + b é: ", soma)
+
+a_str = input('Digite o valor de a: ')
+a_int = int(a_str)
+b_str = input('Digite o valor de b: ')
+b_int = int(b_str)
+soma = a_int + b_int
+print('A soma é: ', soma)
+
