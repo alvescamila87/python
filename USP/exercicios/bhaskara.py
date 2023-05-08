479352a3c9c7ccf8eef6f82ccb3b0934b2b2598a
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-08 20:51:45 -0300

    update fórmula de bhaskara

--- USP/exercicios/bhaskara.py
+++ USP/exercicios/bhaskara.py
@@ -3,22 +3,20 @@ import math
 a=float(input('Informe o valor de a: '))
 b=float(input('Informe o valor de b: '))
 c=float(input('Informe o valor de c: '))
-
 delta = (b ** 2) - 4 * a * c
 
-if a == 0:
-    print('esta equação não possui raízes reais')
+if delta == 0:
+    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+    print('a raiz dupla desta equação é {}'.format(raiz1))
 else:
     if delta < 0:
-        print('a raiz desta equação é {}'.format(delta))
+        print('esta equação não possui raízes reais')
     else:
-        if delta == 0:
-            x0 = - b / (2 * a)
-            print('delta menor que 0,  raiz = {}'.format(x0))
+        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+        if raiz1 < raiz2:
+            print('as raízes da equação são {} e {} '.format(raiz1, raiz2))
         else:
-            x1 = (-b + math.sqrt(delta)) / (2 * a)
-            x2 = (-b - math.sqrt(delta)) / (2 * a)
-
-            print('raízes: x1 = {} x2 = {}'.format(x1, x2))
+            print('as raízes da equação são {} e {} '.format(raiz2, raiz1))
 
 
