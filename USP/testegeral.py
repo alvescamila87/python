54ef8114cc4567076c2a9532d45cfd21af246d33
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-10 18:56:22 -0300

    update aula if else elif

--- USP/testegeral.py
+++ USP/testegeral.py
@@ -1,12 +1,6 @@
-a = 0
-b = 2
-c = 1
+i = 0
 
-if (a > 0):
-    if (b > 0):
-        print ("Tudo ok para decolagem!")
-    else:
-        print ("Tanque principal vazio; voando com combustível reserva!")
-else:
-    if (c > 0):
-        print ("Foguete não tem piloto, mas há outros foguetes disponíveis!")
+while i <=10:
+    print(2 ** i)
+    i = i + 1 #incrementar o valor de 'i'
+
