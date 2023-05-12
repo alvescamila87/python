92aa41c06e15d1cc8ca2e43b39138b56d2c11f5f
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-11 21:22:10 -0300

    update exercicio fatorial

---
+++ USP/exercicios/fatorial.py
@@ -0,0 +1,8 @@
+import math
+
+num = int(input('Digite um número natural: '))
+while num != 0:
+    f = math.factorial(num)
+    print(f)
+    
+
--- USP/testegeral.py
+++ USP/testegeral.py
@@ -4,3 +4,7 @@ while i <=10:
     print(2 ** i)
     i = i + 1 #incrementar o valor de 'i'
 
+count = 0
+while (count <= 10):
+    print (count, "Olá Mundo!")
+    count = count + 1
