e9438dbcb02329271ca77a33c8fd24dc92f0280b
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-16 19:45:19 -0300

    update aula funções python

---
+++ USP/func.py
@@ -0,0 +1,13 @@
+def soma (x, y):
+    return x + y
+
+def multiplica (x, y, z):
+    return x * y * z
+
+def nome_do_seu_time ():
+    return "Corinthians"
+
+def quieta():
+    x = 10 + 20
+    print('O valor de x é: ', x)
+    
