e2441aad45e5cb3d4058b16914359e5903a2ae01
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-05-22 20:04:43 -0300

    update USP Python 'maior_primo' exercício

--- USP/exercicios/maior_primo.py
+++ USP/exercicios/maior_primo.py
@@ -1,3 +1,21 @@
-def maior_primo(n):
-    if n >= 2:
-        return
+
+def éPrimo(k):
+    i = k - 1
+    while i > 0:
+        if i == 1:
+            return True
+            break
+        elif (k % i == 0):
+            return False
+            break
+        else:
+            i -= 1
+
+def maior_primo(x):
+    while x > 0:
+        if éPrimo(x) == True:
+            return x
+            break
+        else:
+            x -= 1
+    return x
