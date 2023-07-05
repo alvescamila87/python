54df23778582da4c00f1d252a73fdc35b04fc267
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-07-04 21:02:43 -0300

    update python while class

---
+++ UdemyHoraDeCodar/21_while.py
@@ -0,0 +1,14 @@
+#while
+
+print("ANTES do Loop")
+
+#início do while
+numero = 0
+
+while numero < 5:
+    print(numero)
+    numero = numero + 1
+#fim do while
+
+
+print("DEPOIS do Loop")
--- UdemyHoraDeCodar/ExerciciosUdemy/exe027.py
+++ UdemyHoraDeCodar/ExerciciosUdemy/exe027.py
@@ -8,8 +8,25 @@ if renda_usuario < 2000:
 elif renda_usuario >= 2000 and renda_usuario < 4000:
     print("Para a renda de R$%.2f, o limite do cartão de crédito é de 'R$2.000,00'." %renda_usuario)
 
-elif renda_usuario >= 4000 and renda_usuario < 6000:
+elif renda_usuario >= 4000 and renda_usuario < 10000:
     print("Para a renda de R$%.2f, o limite do cartão de crédito é de 'R$3.000,00'." %renda_usuario)
     
 else:
     print("Para a renda de R$%.2f, entrar em contato com o gerente da sua conta bancária." %renda_usuario)
+
+#ou pode ser feito:
+
+renda = float(input("Informa o valor da sua renda R$: "))
+limite = 0
+
+if renda < 2000:
+    limite = 1000
+elif renda < 4000:
+    limite = 2000
+elif renda < 10000:
+    limite = 3000
+elif renda > 10000:
+    limite = 3000
+    print("Você precisa falar com  nosso gerente")
+
+print("O seu cartão foi aprovado e o limite é de R$%d" %limite)
