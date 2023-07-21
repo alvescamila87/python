28a5a57d07dc9fb33d7a5bee1ac21051a0cc4eb3
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-07-21 20:06:38 -0300

    update python curso em video ex049 for in

--- CursoEmVideo/Mundo02_Estruturas_Controles/Exercicios/exe049.py
+++ CursoEmVideo/Mundo02_Estruturas_Controles/Exercicios/exe049.py
@@ -1,4 +1,5 @@
 #estrutura de repetição: exercício tabuada v2
+tabuada = int(input("Digite um número para ver a tabuada: "))
 
 for num in range(1,11):
-    print("7 X {} = {}".format(num, (num*7)))
+    print("{} X {:2} = {}".format(tabuada, num, num*tabuada))
