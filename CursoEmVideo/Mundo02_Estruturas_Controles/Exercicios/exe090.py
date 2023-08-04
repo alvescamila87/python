53bf1955ef33f89a3b75e04a02b6666481ed5ec2
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-08-04 20:41:35 -0300

    update python dicionario = {}

--- CursoEmVideo/Mundo02_Estruturas_Controles/Exercicios/exe090.py
+++ CursoEmVideo/Mundo02_Estruturas_Controles/Exercicios/exe090.py
@@ -16,3 +16,22 @@ else:
     aluno["situação"] = "Reprovado"
 
 print(f" - situação é igual a {aluno['situação']}")
+
+
+# Opção 2:
+
+aluno = dict()
+
+aluno["nome"] = str(input("Nome: "))
+aluno["media"] = float(input(f"Média de {aluno['nome']}:  "))
+
+print("-="*30)
+if aluno["media"] >= 7:
+    aluno["situação"] = "Aprovado"
+elif aluno["media"] <= 5 or aluno["media"] < 7:
+    aluno["situação"] = "Recuperação"
+else:
+    aluno["situação"] = "Reprovado"
+
+for k, v in aluno.items():
+    print(f"{k} é igual a {v}")
