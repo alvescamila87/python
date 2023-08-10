324b337d94195fcfa813adabca3ebb8c0a3775e6
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-08-10 11:26:20 -0300

    update ex094

--- CursoEmVideo/Mundo02_Estruturas_Controles/Exercicios/exe094.py
+++ CursoEmVideo/Mundo02_Estruturas_Controles/Exercicios/exe094.py
@@ -2,6 +2,8 @@
 
 dados = dict()
 pessoas = list()
+soma = 0
+media = 0
 
 while True:
      dados["nome"] = str(input("Nome: "))
@@ -10,6 +12,7 @@ while True:
           sexo = str(input("ERRO! Repita apenas M ou F. \nSexo: [M/F] ")).strip().upper()[0]
      dados["sexo"] = sexo
      dados["idade"] = int(input("Idade: "))
+     soma += dados["idade"]
      pessoas.append(dados.copy())
      continuar = str(input("Quer continuar? [S/N]")).strip().upper()[0]
      while continuar not in "SN":
@@ -20,9 +23,20 @@ while True:
 print("LISTA: ", pessoas)
 print("DICIONADO: ", dados)
 
-print(f"Ao todo temos {len(pessoas)} pessoas cadastradas.")
-for pessoa in pessoas:
-     for v in pessoa.values():
-          print(f'{v}')
-     print()
-#print(f"A média de idade é de {media} anos")
+print(f"A) Ao todo temos {len(pessoas)} pessoas cadastradas.")
+media = soma / len(pessoas)
+print(f"B) A média de idade é de {media:5.2f} anos.")
+print(f"C) A mulheres cadastradas foram ", end="")
+for p in pessoas:
+     if p['sexo'] == 'F':
+          print(f"{p['nome']} ", end="")
+print()
+print("D) Lista das pessoas que estão acima da média: ")
+for p in pessoas:
+     if p['idade'] >= media:
+          print('    ', end="")
+          for k, v in p.items():
+               print(f"{k} = {v}; ", end="")
+          print()
+print('<< ENCERRADO >>')
+
