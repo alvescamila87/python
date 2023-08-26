76c7a9dc627cd988d55ae2b20c2cd14b5fd6a9d7
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-08-25 22:24:20 -0300

    update herança e polimorfismo

--- Dunossauro/Live061_OOP/aula02_cls_self.py
+++ Dunossauro/Live061_OOP/aula02_cls_self.py
@@ -1,3 +1,65 @@
-# OOP: Herança e Polimorfismo
+# OOP: Diferença entre CLS e SELF
 
-# 1)  Herança
+# 1)  Diferença entre CLS e SELF - Exemplo 1
+class Fila:
+    """Abstração de qualquer tipo de Fila:
+        - supermercado
+        - processos
+        - ...
+    """
+
+    # (CLS) Compartilhado
+    # Escopo compartilhado
+    c_fila = []
+
+    @classmethod
+    def c_entrar(cls, objeto):
+        cls.c_fila.append(objeto)
+        print(cls.c_fila)
+
+    # (Self) Específico
+    # Escopo Específico
+    def __int__(self):
+        self.s_fila = []
+
+    def s_entrar(self, objeto):
+        self.s_fila.append(objeto)
+        print(self.s_fila)
+
+banco = Fila()
+supermercado = Fila()
+processos = Fila()
+banco.c_fila
+banco.c_entrar("Guilherme") # Escopo compartilhado
+banco.c_fila # Escopo compartilhado
+supermercado.c_fila # Escopo compartilhado
+processos.c_fila # Escopo compartilhado
+
+
+banco.s_entrar("João") # Escopo específico
+supermercado.s_entrar("Camila") # Escopo específico
+banco.s_fila # Escopo específico
+supermercado.s_fila # Escopo específico
+processos.s_fila # Escopo específico
+
+
+
+# 1)  Diferença entre CLS e SELF - Exemplo 2
+
+class Pizza:
+    pecados = 8
+
+    def __int__(self, sabor):
+        self.sabor = sabor
+
+    def pegar_pedaco(self): # Escopo específico
+        "Método de instância"
+        self.pecados -= 1
+
+    @classmethod # Escopo compartilhado
+    def mudar_tamanho(cls, pedacos):
+        cls.pedaco = pedacos
+
+    @staticmethod
+    def ingredientes(): # Método / Função aleatória que independe dos demais
+        return "Molho de tomate, queijo e cebola"
--- Dunossauro/Live061_OOP/aula02_heranca_polimorfismo.py
+++ Dunossauro/Live061_OOP/aula02_heranca_polimorfismo.py
@@ -1,65 +1,46 @@
-# OOP: Diferença entre CLS e SELF, Herança e Polimorfismo
+# OOP: Herança e Polimorfismo
 
-# 1)  Diferença entre CLS e SELF - Exemplo 1
-class Fila:
-    """Abstração de qualquer tipo de Fila:
-        - supermercado
-        - processos
-        - ...
-    """
-
-    # (CLS) Compartilhado
-    # Escopo compartilhado
-    c_fila = []
-
-    @classmethod
-    def c_entrar(cls, objeto):
-        cls.c_fila.append(objeto)
-        print(cls.c_fila)
-
-    # (Self) Específico
-    # Escopo Específico
-    def __int__(self):
-        self.s_fila = []
-
-    def s_entrar(self, objeto):
-        self.s_fila.append(objeto)
-        print(self.s_fila)
-
-banco = Fila()
-supermercado = Fila()
-processos = Fila()
-banco.c_fila
-banco.c_entrar("Guilherme") # Escopo compartilhado
-banco.c_fila # Escopo compartilhado
-supermercado.c_fila # Escopo compartilhado
-processos.c_fila # Escopo compartilhado
-
-
-banco.s_entrar("João") # Escopo específico
-supermercado.s_entrar("Camila") # Escopo específico
-banco.s_fila # Escopo específico
-supermercado.s_fila # Escopo específico
-processos.s_fila # Escopo específico
-
-
-
-# 1)  Diferença entre CLS e SELF - Exemplo 2
-
+# 1)  Herança:
 class Pizza:
     pecados = 8
 
-    def __int__(self, sabor):
-        self.sabor = sabor
+    @classmethod # Escopo compartilhado
+    def mudar_tamanho(cls, pedacos):
+        cls.pedaco = pedacos
 
-    def pegar_pedaco(self): # Escopo específico
-        "Método de instância"
-        self.pecados -= 1
+class FrangoCatupiry(Pizza):
+    pass
+
+class Pepperoni(Pizza):
+    pass
+
+class BananaNevada(Pizza):
+    pass
+
+# Herança múltipla
+class MeioAMeio(FrangoCatupiry, Pepperoni):
+    pass
+
+# 2)  Polimorfismo: (sobrescrever / overwriting) para reuso de software
+
+class Pizza:
+    pecados = 8
 
     @classmethod # Escopo compartilhado
     def mudar_tamanho(cls, pedacos):
         cls.pedaco = pedacos
 
     @staticmethod
-    def ingredientes(): # Método / Função aleatória que independe dos demais
-        return "Molho de tomate, queijo e cebola"
+    def ingredientes():
+        return "Ingredientes"
+
+class FrangoCatupiry(Pizza):
+    @staticmethod
+    def ingredientes():
+        return ['queijo',
+                'catupiry',
+                'frango']
+
+print(Pizza.ingredientes)
+print(Pizza.ingredientes()) # herdou pizza
+print(FrangoCatupiry.ingredientes()) # herdou pizza, mas sobrescreveu ingredientes
