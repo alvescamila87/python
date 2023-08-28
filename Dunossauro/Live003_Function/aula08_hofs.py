2330204f826108aad0c7b8dcbbf2bbbbdc12eb4b
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-08-27 21:50:34 -0300

    update módulo operator

---
+++ Dunossauro/Live003_Function/aula08_hofs.py
@@ -0,0 +1,18 @@
+# Funções #8 - Módulo operator
+
+"""Operator."""
+from operator import add, mul, sub, floordiv, itemgetter
+from functools import reduce
+
+print("Utilizando lambda: ", lambda x, y: x + y, [1,2,3,4,5])
+print("Utilizando reduce, operator add: ", reduce(add, [1,2,3,4,5]))
+print("Utilizando reduce, operador sub: ", reduce(sub, [1,2,3,4,5]))
+print("Utilizando reduce, operador mul: ", reduce(mul, [1,2,3,4,5]))
+print("Utilizando reduce, operador div: ", reduce(floordiv, [200, 4, 5]))
+
+# Opção 2
+
+palavras = ["amar", "falar", "ler", "meditar", "rezar", "abacate"]
+
+print("Modo exemplo 1 - lambda: ", sorted(palavras, key=lambda string: string[1]))
+print("Modo exemplo 2 - itemgetter: ", sorted(palavras, key=itemgetter(1)))
