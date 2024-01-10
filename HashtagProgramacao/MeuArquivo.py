70b6dc00e26ea4aea2c1dc98b72384696f0a7780
Camila <116912821+alvescamila87@users.noreply.github.com>
2024-01-10 19:14:15 -0300

    criando tabelas com pandas, gerando dados de faturamento, quantidade e ticket medio

--- HashtagProgramacao/MeuArquivo.py
+++ HashtagProgramacao/MeuArquivo.py
@@ -2,14 +2,24 @@ import pandas as pd
 
 # importar base de dados
 tabela_vendas = pd.read_excel('vendas.xlsx')
-print(tabela_vendas)
 
 # visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabela_vendas)
 
-# faturamento por loja
+print('-' * 50)
+# faturamento por loja (filtra, agrupa e soma)
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
 
+print('-' * 50)
 # quantidade de produtos por loja
+quantidade_produtos = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade_produtos)
 
+print('-' * 50)
 # ticket medio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade_produtos['Quantidade']).to_frame()
+print(ticket_medio)
 
 # enviar um email com o relatório
