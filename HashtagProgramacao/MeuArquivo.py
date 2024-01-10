4fa8159cff165fca720368b464f84b38ae43f24c
Camila <116912821+alvescamila87@users.noreply.github.com>
2024-01-10 16:58:01 -0300

    projeto minicursos hashtag python: importar tabela de vendas, instalar bibliotecas

---
+++ HashtagProgramacao/MeuArquivo.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# importar base de dados
+tabela_vendas = pd.read_excel('vendas.xlsx')
+print(tabela_vendas)
+
+# visualizar a base de dados
+
+# faturamento por loja
+
+# quantidade de produtos por loja
+
+# ticket medio por produto em cada loja
+
+# enviar um email com o relatório
